7fd11a287d29e314646d15ea6336c7bf90600468
Anthonyy Choque <giananthoych@outlook.es>
2024-03-03 17:41:52 -0500

    Tablas con bootstab para practicar TS

---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,30 @@
+import { Curso } from "src/app/models/curso-to";
+
+export const cursos:Curso[] = [
+{
+    materia: 'Javascript',
+    dificultad: 9,
+    requiere: 'HTML y CSS'
+},
+{
+    materia: 'Typescript',
+    dificultad: 7,
+    requiere: 'Javascript'
+},
+{
+    materia: 'Angular',
+    dificultad: 9,
+    requiere: 'HTML, CSS, JS y TS'
+},
+{
+    materia: 'React',
+    dificultad: 7,
+    requiere: 'HTML, CSS, JS'
+},
+{
+    materia: 'Spring Boot',
+    dificultad: 9,
+    requiere: 'POO, Java, SQL'
+}
+
+]
